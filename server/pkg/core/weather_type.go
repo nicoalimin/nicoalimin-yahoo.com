d303f1c047ee7b17b4bd2313f4aa7e7024cbbca7
nicoalimin <nicoalimin@yahoo.com>
2020-03-31 14:51:13 -0700

    Added comments and better interface for the weathers API

--- server/pkg/core/weather_type.go
+++ server/pkg/core/weather_type.go
@@ -1,10 +1,24 @@
 package core
 
+import (
+	"errors"
+)
+
+const (
+	// ErrCityNotFound is the error if the given city is invalid
+	ErrCityNotFound = errors.New("Desired city is not found")
+)
+
+// Weather contains a location and the current summary of the weather
 type Weather struct {
-	City        string
-	Temperature string
+	City        string // location of the desired weather check
+	Temperature string // temperature of a given location
+	Condition   string // whether given location is sunny, rainy, cloudy, etc.
 }
 
+// CoreWeather contains business logic of the Weather's API
 type CoreWeather interface {
-	GetWeatherByCity(city string) *Weather
+	// GetWeatherByCity obtains a particular city's current weather. Returns a weather
+	// object or an error.
+	GetWeatherByCity(city string) (*Weather, error)
 }
