50f10e04f3c5f38de2efb4c7ee3777cdb3a504b2
nicoalimin <nicoalimin@yahoo.com>
2020-03-31 15:41:25 -0700

    Added OpenWeather response and error codes

--- server/pkg/core/weather_type.go
+++ server/pkg/core/weather_type.go
@@ -4,16 +4,33 @@ import (
 	"errors"
 )
 
-const (
+var (
 	// ErrCityNotFound is the error if the given city is invalid
-	ErrCityNotFound = errors.New("Desired city is not found")
+	ErrCityNotFound = errors.New("desired city is not found")
+	// ErrBadRequest gets thrown if the request does not come with the necessary arguments
+	ErrBadRequest = errors.New("error validating the parameters")
+	// ErrQueryingExternalWeathersAPI gets thrown if there is an error from the external
+	// weathers API
+	ErrQueryingExternalWeathersAPI = errors.New("error querying external weathers API")
 )
 
 // Weather contains a location and the current summary of the weather
+// TODO: Improve Weather so it returns valuable weather info too (sunrise, humidity, etc)
 type Weather struct {
 	City        string // location of the desired weather check
 	Temperature string // temperature of a given location
 	Condition   string // whether given location is sunny, rainy, cloudy, etc.
+}
+
+// OpenWeatherResponse is the external Weathers API that is used to query weathers by city
+type OpenWeatherResponse struct {
+	Weather []struct {
+		ID   int    `json:"id"`
+		Main string `json:"main"`
+	} `json:"weather"`
+	Main struct {
+		Temp float64 `json:"temp"`
+	} `json:"main"`
 }
 
 // CoreWeather contains business logic of the Weather's API
